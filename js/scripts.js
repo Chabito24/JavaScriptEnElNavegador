a6dc2b6795940a8ac82fa01837dec364e78ed841
Edward Martinez <emartinezj1802@gmail.com>
2025-08-06 13:52:01 -0500

    se crea un validador para el formulario generando mensaje de error y de envio segun corresponda

--- js/scripts.js
+++ js/scripts.js
@@ -84,16 +84,20 @@ window.onscroll = function() {
 
 // === ASOCIAR EVENTOS A ELEMENTOS (148) ===
 
-const btnEnviar = document.querySelector('.boton--primario');
+/*const btnEnviar = document.querySelector('.boton--primario');
 
 btnEnviar.addEventListener('click', function(e) {//e puede ser cualquier nombre, hace referencia al evento.
     console.log(e); // Imprime el objeto del evento
     e.preventDefault(); // Evita que el formulario se envíe automáticamente, lo uso par avalidar un formulario.
     console.log('formulario enviado'); // Simulación de envío (por ahora solo consola)
-});
+});*/
 
 
 // === EVENTOS DE LOS INPUTS Y TEXTAREA (149) ===
+
+//evento de submit
+
+
 
 const datos = { 
     nombre: '',
@@ -104,6 +108,7 @@ const datos = {
 const nombre = document.querySelector('#nombre');// Seleccionamos el input con el ID "nombre"
 const email = document.querySelector('#email');// Seleccionamos el input con el ID "email"
 const mensaje = document.querySelector('#mensaje');// Seleccionamos el input con el ID "mensaje"
+const formulario = document.querySelector('.formulario');
 
 /*// Evento que se dispara cada vez que el usuario escribe en el campo
 nombre.addEventListener('input', function(e) {
@@ -124,15 +129,71 @@ nombre.addEventListener('input', leerTexto);
 email.addEventListener('input', leerTexto);
 mensaje.addEventListener('input', leerTexto); // seleccionamos los id que requerimos
 
+//envento de submit 
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    //validar el formulario
+
+    const {nombre, email, mensaje} = datos// aplicamos destructuring
+
+    if(nombre ==='' || email ==='' || mensaje ===''){
+        mostrarError('Todos los campos son obligatorios');
+    
+        return;
+         //enviar el formulario
+
+    }
+
+     mostrarEnvio('Formulario enviado');  
+});
+
+
+
+
+
+
 function leerTexto(e) {
     //console.log(e.target.value);
     datos[e.target.id] = e.target.value; // asigna los valores de los campos en el evento, toma en cuenta el id 
     // console.log(e.target);
-    console.log(datos); // imprime los datos con el valor del id.
+    //console.log(datos); // imprime los datos con el valor del id.
 } // imprime en consola cada dato incorporado en el formulario de nombre a mensaje
 
-btnEnviar.addEventListener('click', function() {
+/*btnEnviar.addEventListener('click', function() {
     console.clear();
-    console.log('se envia formulario')
-}) // limpia la consola e imprime el mensaje de envío
+    console.log('se envia formulario') // este esta asociado al boton no al formulari para el formulario se usa submit ver linea 98
+})*/ // limpia la consola e imprime el mensaje de envío
+
+
+
+//muestra un error en pantalla
+function mostrarError(mensaje) {
+    const error = document.createElement('p');
+    error.textContent = mensaje;
+    error.classList.add('error');
+
+    formulario.appendChild(error);
+
+    //desaparezca despues de 5 segundos
+
+    setTimeout(()=>{
+        error.remove();
+    }, 3000);
+}
+
+function mostrarEnvio(mensaje) {
+    const correcto = document.createElement('p');
+    correcto.textContent = mensaje;
+    correcto.classList.add('correcto');
+
+    formulario.appendChild(correcto);
+
+    setTimeout(()=>{
+        correcto.remove();
+    }, 3000);
+}
+
+
 
